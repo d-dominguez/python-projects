b2b6db6714e06f594753cab27d3abba41a031a4d
David Dominguez <david.dominguez@berkeley.edu>
2018-08-08 22:47:03 -0700

    Update health_insurance_app.py

--- health_insurance_app/health_insurance_app.py
+++ health_insurance_app/health_insurance_app.py
@@ -1,20 +1,34 @@
+# **** ENTER YOUR USER ID **** & **** ENTER YOUR GOOGLE API KEY **** must be replaced first.
+
+# *** Python 3.6.5 |Anaconda, Inc.| (default, Apr 29 2018, 16:14:56) ***
+
+# USPS API
+# http://production.shippingapis.com/
+
+# Google Maps API
+# google maps api key AIzaSyCnCX__gRDT6Qnazwfz9SkF6n8CvLCBIYI
+
+# US Census API
+# https://www.census.gov/data/developers/data-sets/Health-Insurance-Statistics.html
+# https://api.census.gov/data/timeseries/healthins/sahie
+
 import requests
+import math
 import xmltodict as xm
 import pandas as pd
 import numpy as np
-import itertools as it
 import ast
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
 
-# '**** ENTER YOUR USER ID ****' & '**** ENTER YOUR GOOGLE API KEY ****' must be replaced first.
 
+# Function asks the user to input a zip code and checks that the input is
+# numeric and exactly 5 digits
 def getInput():
-    user_input = input('Please enter zip code:\n')
-    user_input2 = input('Please re-enter zip code:\n')
+    user_input = input('Please enter zip code:\n>>>> ')
+    user_input2 = input('Please re-enter zip code:\n>>>> ')
     if ((len(user_input) is not 5)):
         print('\n***PLEASE CHECK THE ZIP CODE!!*** Input must be 5 digits long.',
-              'Your input was ',len(user_input),' characters long!!')
+              'Your input was ', len(user_input),' characters long!!')
     elif ((user_input.isdigit()&user_input2.isdigit())is not True):
         print('\n***PLEASE CHECK THE ZIP CODE!!*** Input must be numeric!! Your inputs were: ',
              user_input,' & ',user_input2)
@@ -32,30 +46,39 @@ def getInput():
               '\n...............')
     return user_input
 
+
+# Function takes zip code returnd by getInput() and checks if zip code is valid and returns the city
+# and state associated with the zip code using the USPS API
 def validateZip(zip):
-    payload="http://production.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&XML=<CityStateLookupRequest USERID='**** ENTER YOUR USER ID ****'><ZipCode ID='0'><Zip5>"+zip+"</Zip5></ZipCode></CityStateLookupRequest>"
+    payload = "http://production.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&XML=<CityStateLookupRequest USERID='**** ENTER YOUR USER ID ****'><ZipCode ID='0'><Zip5>" + zip + "</Zip5></ZipCode></CityStateLookupRequest>"
     r = requests.get(payload)
     response = xm.parse(r.text)
     city = response['CityStateLookupResponse']['ZipCode']['City']
     state = response['CityStateLookupResponse']['ZipCode']['State']
     city.strip()
     state.strip()
-    print('Zip Code '+zip+' is located in '+city+', '+state)
+    print('\nZip Code '+zip+' is located in '+city+', '+state)
     return city + ', ' + state
 
+
+# Function takes the city and state returned by validateZip() and queries returns
+# the county using the Google Maps API 
 def getCounty(zip):
     payload = ''.join(["https://maps.googleapis.com/maps/api/geocode/xml?address=",
                        user_input,
-                       "'**** ENTER YOUR GOOGLE API KEY ****'"])
+                       "**** ENTER YOUR GOOGLE API KEY ****"])
     r = requests.get(payload)
     response = xm.parse(r.text)
     county = response['GeocodeResponse']['result']['address_component'][2]['long_name']
     state_s = response['GeocodeResponse']['result']['address_component'][3]['short_name']
     state_l = response['GeocodeResponse']['result']['address_component'][3]['long_name']
-    county = county.replace(' County','')
+    county = county.replace(' County', '')
     county = str(county + ', ' + state_s)
-    return {'county':county, 'state_s':state_s ,'state_l':state_l }
+    return {'county' : county, 'state_s' : state_s, 'state_l' : state_l}
 
+
+# Function takes county returned by getCounty() and returns
+# the county and state codes used by the Census Bureau
 def getStateCountyCodes(dictionary):
     codes = pd.read_csv('https://lehd.ces.census.gov/data/schema/latest/label_geography.csv')
     county = dictionary['county']
@@ -65,6 +88,10 @@ def getStateCountyCodes(dictionary):
     county_code = county_code[2:]
     return {'state_code':state_code, 'county_code':county_code}
 
+
+# Function takes city and county codes returned by getStateCountyCodes() and returns
+# health insurance coverage in the county corresponding to the original zip code 
+# entered by the user using the Census Bureau API
 def getCensusData(dict1):
     state_code = str(dict1['state_code'])
     county_code = str(dict1['county_code'])
@@ -80,76 +107,79 @@ def getCensusData(dict1):
     df = pd.Series(response)
     df = pd.DataFrame(df, columns = ["data"])
     df = pd.DataFrame(df.data.values.tolist()).add_prefix('__')
-    new_header = df.iloc[0] #grab the first row for the header
-    df = df[1:] #take the data less the header row
-    df.columns = new_header #set the header row as the df header
+    new_header = df.iloc[0] 
+    df = df[1:] 
+    df.columns = new_header 
     df.iloc[:,1:6] = df.iloc[:,1:6].apply(pd.to_numeric)
     df.columns = ['Income-to-Poverty Ratio Group','Insured', 'Uninsured','Year','State Code', 'County Code']
     df['Uninsured %'] = df['Uninsured']/(df['Insured']+df['Uninsured'])
     return df
 
-def generateBarChart_total(df):
-    bar_width=0.45
-    opacity = 0.4
 
-    plt.xlabel('')
-    plt.ylabel('Total in Millions')
-
-    mx = max(df['Insured'])*1.3
-    mx = int(mx)
-
-    values = np.arange(0,mx,200000)
-    value_increment = 1
-    bar1 = plt.bar(np.arange(len(df['Uninsured']))+ bar_width, df['Uninsured'], bar_width, align='center', alpha=opacity, color='r', label='Uninsured')
-    bar2 = plt.bar(range(len(df['Insured'])), df['Insured'], bar_width, align='center', alpha=opacity, color='g', label='Insured')
-
-    plt.yticks(values * value_increment, ['%s' % round(val,0) for val in values])
-    plt.xticks(range(len(df['Income-to-Poverty Ratio Group'])),('Group\n1', 'Group\n2', 'Group\n3', 'Group\n4', 'Group\n5','Group\n6'))
+# Generates a chart with total # of insured and uninsured
+# grouped by Income-to-Poverty Ratio Group
+def generateBarChart_total(df): 
+    insured = df['Insured']
+    uninsured = df['Uninsured']
+    
+    ind = np.arange(len(df['Income-to-Poverty Ratio Group']))
+    bar_width = 0.45
+    
+    fig, ax = plt.subplots()
+    fig.set_size_inches(11, 8.5)
+    bar1 = ax.bar(ind, insured, bar_width, align = 'center', alpha = 0.4 , color ='g', label = 'Insured')
+    bar2 = ax.bar(ind + bar_width, uninsured, bar_width, align = 'center', alpha = 0.4 , color = 'r', label = 'Uninsured')
+    
+    county = county_results['county']
+    ax.set_ylabel('Population', fontweight="bold")
+    ax.set_title('Population with Health Insurance in \n' + county[:-4] + ' County', fontsize=16, fontweight="bold")
+    ax.legend()
+  
+    mx = int(max(df['Insured'])*1.2)
+    values = np.arange(0, mx, math.ceil((mx /20)/ 200000) * 200000)
+    plt.yticks(values, ['%s' % ("{:,}".format(val)) for val in values])
+    ax.set_xticks(ind + bar_width / 2)
+    ax.set_xticklabels(('Group\n1', 'Group\n2', 'Group\n3', 'Group\n4', 'Group\n5','Group\n6'))
 
     for rect in bar1 + bar2:
-        height = rect.get_height()
-        plt.text(rect.get_x() + rect.get_width()/2.0, height, '%s' % int(height), ha='center', va='bottom')
-
-    plt.legend()
-    fig_size = plt.rcParams["figure.figsize"]
-    fig_size[0] = 12
-    fig_size[1] = 8
-    plt.rcParams["figure.figsize"] = fig_size
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width()/2.0, height, '%s' % ("{:,}".format(height)), ha='center', va='bottom')  # ("{:,}".format(val)
     plt.show()
 
-def generateBarChart_percent(df):  
-        bar_width=0.45
-        opacity = 0.4
 
-        plt.xlabel('')
-        plt.ylabel('% Uninsured')
-
-        mx = (max(df['Uninsured %']))*1.3
-
-        values = np.arange(0.0,mx,.01)*100
-        value_increment = .10
-        bar1 = plt.bar(np.arange(len(df['Uninsured %']*100))+ bar_width, df['Uninsured %']*100, bar_width, align='center', alpha=opacity, color='r', label='Uninsured %')
-
-
-        plt.yticks(values, ['%s' % round(val,2) for val in values])
-        plt.xticks(range(len(df['Income-to-Poverty Ratio Group'])),('Group\n1', 'Group\n2', 'Group\n3', 'Group\n4', 'Group\n5','Group\n6'))
-
-        for rect in bar1:
-            height = round(rect.get_height(),2)
-            plt.text(rect.get_x() + rect.get_width()/2, height, '%s' % height, ha='center', va='bottom')
-
-        plt.legend()
-        fig_size = plt.rcParams["figure.figsize"]
-        fig_size[0] = 12
-        fig_size[1] = 8
-        plt.rcParams["figure.figsize"] = fig_size
-        plt.show()
-  
+# Generates a chart with percentage uninsured grouped by Income-to-Poverty Ratio Group
+def generateBarChart_percent(df): 
+    insured_per = df['Uninsured %']
+    
+    ind = np.arange(len(df['Income-to-Poverty Ratio Group']))
+    bar_width = 0.75
+    
+    fig, ax = plt.subplots()
+    fig.set_size_inches(11, 8.5)
+    bar1 = ax.bar(ind, insured_per, bar_width, align = 'center', alpha = 0.4 , color ='r', label = '% Unsured')
+    
+    county = county_results['county']
+    ax.set_ylabel('% of Population', fontweight="bold")
+    ax.set_title('% Population with Health Insurance in \n' + county[:-4] + ' County', fontsize=16, fontweight = "bold")
+    ax.legend()
+    
+    mx = max(df['Uninsured %']) * 1.2    
+    values = np.arange(0, mx, math.ceil((mx / 20) / 0.02) * 0.02)
+    plt.yticks(values, ['%s' % round(val * 100, 2) for val in values])
+    ax.set_xticks(ind)
+    ax.set_xticklabels(('Group\n1', 'Group\n2', 'Group\n3', 'Group\n4', 'Group\n5','Group\n6'))
+    
+    for rect in bar1:
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width()/2, height, '%s' % round(height*100,2), ha='center', va='bottom')
+    
+    plt.show()
+    
+    
 user_input = getInput()
 city_state = validateZip(user_input)
 county_results = getCounty(user_input)
 codes = getStateCountyCodes(county_results)
 insurance_data = getCensusData(codes)
 generateBarChart_total(insurance_data)
-enerateBarChart_percent(insurance_data)
-
+generateBarChart_percent(insurance_data)
